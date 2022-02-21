2ab2684af008c51f35b6246185ca91ef891e4d62
Jonathan <jms15af@my.fsu.edu>
2022-02-21 16:23:46 -0500

    update readme. update docstrings. reorder function

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # wordle_analysis
-script to find the optimal starting word
+Script to find the optimal starting word
+**Notes** Strictly for python practice only!
+Requires a list of words to work on in a text file called worlde_words.txt.
+This script only tries to maximize the number of green letters a starting word will get, which may not actually be the optimal starting word strategy.
+
+##ranking method
+Each word is assigned a score.
+For a given word its score will be the sum of the number of words that have the same letter in the same space.
+e.g.
+There are 1560 letters that start with 's',
+There are 176 letters that have 'c' in the second position,
+There are 989 letters that have 'o' in the third position,
+There are 716 letters that have 'r' in the fourth position,
+There are 1519 letters that have 'e' in the fifth position,
+So the score for the word "score" is 4960.
--- wordle_analysis.py
+++ wordle_analysis.py
@@ -3,6 +3,11 @@
 def get_letter_index(letter):
     """get index of letter (a = 0, z = 25)"""
     return ord(letter) - ord("a")
+
+def word_repeats_letters(word):
+    """returns true if a word has repeated letters"""
+    return len(set(word)) != len(word)
+
 def absolute_letter_frequency(word_list):
     """returns list of tuples (letter, letter frequency) in order by most used"""
     char_freq = [0] * 26
@@ -13,28 +18,21 @@ def absolute_letter_frequency(word_list):
     char_freq = map(lambda x: (chr(x[0] + ord("a")), x[1]), enumerate(char_freq))
     char_freq = sorted(char_freq, key=lambda x: x[1])[::-1]
     return char_freq
+
 def positional_letter_frequency(word_list):
-    """should return a 26 * 5 matrix of which letters appear most frequently in which spaces"""
+    """returns a 5 * 26 matrix of which letters appear most frequently in which spaces"""
     ret = [[0 for _ in range(26)] for _ in range(5)]
     for word in word_list:
         for i, letter in enumerate(word):
             ret[i][get_letter_index(letter)] += 1
     return ret
+
 def get_word_rankings(word_list):
     """get words most likely to have green letters
-    ranking method:
-    assign each word a score
-    for a given word its score will be the sum of the number of words that have
-        the same letter in the same space
-    e.g.
-    there are 1560 letters that start with 's'
-    there are 176 letters that have 'c' in the second position
-    there are 989 letters that have 'o' in the third position
-    there are 716 letters that have 'r' in the fourth position
-    there are 1519 letters that have 'e' in the fifth position
-    so the score for the word "score" is 4960
-    """
+    returns list of tuples (word, score)"""
     matrix = positional_letter_frequency(word_list)
+    #todo make repeated letters an option
+    #todo make words that end with S an option
     no_repeated_letter_words = list(filter(lambda x: not word_repeats_letters(x), words))
     scores = []
     for word in no_repeated_letter_words:
@@ -43,9 +41,7 @@ def get_word_rankings(word_list):
             score += matrix[i][get_letter_index(letter)]
         scores.append((word, score))
     return sorted(scores, key=lambda x: x[1])[::-1]
-def word_repeats_letters(word):
-    """returns if a word has repeated letters"""
-    return len(set(word)) != len(word)
+
 if __name__ == "__main__":
     with open (r"wordle_words.txt", "r", encoding="UTF-8") as word_file:
         words = word_file.readlines()
